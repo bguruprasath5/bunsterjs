51884490cd639cd106282f0764be42787bf28bf7
dev-guruprasath <96348958+dev-guruprasath@users.noreply.github.com>
2023-09-27 18:24:10 +0530

    added server, client libraries

--- examples/src/client/client.ts
+++ examples/src/client/client.ts
@@ -1,5 +1,5 @@
 import { createApiClient } from "@bunsterjs/client";
-import type { ApiRouter } from "./server/main";
+import type { ApiRouter } from "../server/main";
 
 const client = createApiClient<ApiRouter>("http://localhost:4040");
 try {
